10f50e51820ad848485410726b6c5827508e5176
Kwabena Ntiamoah Boateng <kboatb0@gmail.com>
2025-03-27 16:58:17 +0000

    Connect to server

---
+++ MessagingClient/src/client.cpp
@@ -0,0 +1,59 @@
+#include "client.h"
+
+Client::Client()
+{
+    connectToServer();
+}
+
+
+Client::~Client()
+{
+    closesocket(clientSocket);
+}
+
+
+void Client::connectToServer()
+{
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(8080);
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+
+    while (true) {
+        clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+        if (clientSocket == INVALID_SOCKET) {
+            std::cerr << "Socket creation failed: " << WSAGetLastError() << '\n';
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+            continue;
+        }
+
+        if (connect(clientSocket, (sockaddr*)&serverAddress, sizeof(serverAddress)) == SOCKET_ERROR) {
+            std::cerr << "Connection failed: " << WSAGetLastError() << '\n';
+            closesocket(clientSocket);
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+        }
+        else {
+            std::cout << "Connected to server!\n";
+            break;
+        }
+    }
+
+    std::string username;
+    std::cout << "Enter username: ";
+    std::getline(std::cin, username);
+
+    bool sent = false;
+    while (!sent) {
+        if (send(clientSocket, username.c_str(), username.size(), 0) == SOCKET_ERROR) {
+            std::cerr << "Failed to send USERNAME: " << WSAGetLastError() << std::endl;
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+            closesocket(clientSocket);
+            connectToServer();
+            return;
+        }
+        else {
+            sent = true;
+        }
+    }
+}
+
+
