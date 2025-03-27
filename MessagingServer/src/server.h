17b1a449ac40f506037c0ee73345ef25926bd10f
Kwabena Ntiamoah Boateng <kboatb0@gmail.com>
2025-03-27 16:35:00 +0000

    Server-side

--- MessagingServer/src/server.h
+++ MessagingServer/src/server.h
@@ -1,1 +1,27 @@
 #pragma once
+
+#include <winsock2.h>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <thread>
+#include <mutex>
+#include <algorithm>
+
+class Server {
+public:
+	Server();
+	~Server();
+
+	void startServer();
+	void handleClients(SOCKET sockclient);
+	void acceptClients();
+
+private:
+	SOCKET serverSocket;
+	SOCKET clientSocket;
+	sockaddr_in serverAddress;
+	std::vector<SOCKET> clientsContainer;
+	std::unordered_map<SOCKET, std::string> socketToUserName;
+	std::mutex clientMutex;
+};
