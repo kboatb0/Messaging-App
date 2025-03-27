9fcd6df21457e65bdbc317aab456e44ff9690994
Kwabena Ntiamoah Boateng <kboatb0@gmail.com>
2025-03-27 16:49:01 +0000

    Client-side

--- MessagingClient/src/client.h
+++ MessagingClient/src/client.h
@@ -1,1 +1,18 @@
 #pragma once
+
+#include <iostream>
+#include <winsock2.h>
+#include <thread>
+#include <string>
+
+class Client {
+public:
+	Client();
+	~Client();
+	void connectToServer();
+	void sendMessage();
+	void receiveMessage();
+private:
+	SOCKET clientSocket;
+	sockaddr_in serverAddress;
+};
